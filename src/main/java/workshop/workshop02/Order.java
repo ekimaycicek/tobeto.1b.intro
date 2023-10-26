832db1f6414d180f66e36d90827bbc927e88002a
ekimaycicek <ekimaycicek@icloud.com>
2023-10-26 21:12:54 +0300

    new

--- src/main/java/workshop/workshop02/Order.java
+++ src/main/java/workshop/workshop02/Order.java
@@ -1,4 +1,30 @@
 package workshop.workshop02;
 
 public class Order {
+    private int id;
+    private int quantity;
+
+
+    public Order(int id, int quantity) {
+        this.id = id;
+        this.quantity = quantity;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
 }
