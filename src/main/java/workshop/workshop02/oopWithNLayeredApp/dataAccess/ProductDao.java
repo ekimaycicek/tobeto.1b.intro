80d49ef9e9d585529327a606318919bd8dcb4d5d
ekimaycicek <ekimaycicek@icloud.com>
2023-10-27 12:39:51 +0300

    new

---
+++ src/main/java/workshop/workshop02/oopWithNLayeredApp/dataAccess/ProductDao.java
@@ -0,0 +1,9 @@
+package workshop.workshop02.oopWithNLayeredApp.dataAccess;
+
+
+import workshop.workshop02.oopWithNLayeredApp.entities.Product;
+
+public interface ProductDao {
+
+        void add(Product product);
+}
