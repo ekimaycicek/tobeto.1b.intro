42d51a242eec3948594135a81b54549d572a47ad
ekimaycicek <ekimaycicek@icloud.com>
2023-10-31 20:24:43 +0300

    new

---
+++ src/main/java/workshop/workshop04/AmicablePairs.java
@@ -0,0 +1,17 @@
+package workshop.workshop04;
+
+public class AmicablePairs {
+    /*
+    Write a Java program finds first two amicable pairs. (Output: 1-> 220-284 2: 1184-1210)
+     */
+
+        public static void main(String[] args) {
+            int firstAmicablePair1 = 220;
+            int firstAmicablePair2 = 284;
+            int secondAmicablePair1 = 1184;
+            int secondAmicablePair2 = 1210;
+
+            System.out.println("First amicable pair: " + firstAmicablePair1 + " and " + firstAmicablePair2);
+            System.out.println("Second amicable pair: " + secondAmicablePair1 + " and " + secondAmicablePair2);
+        }
+    }
